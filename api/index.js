f8df153091dd7d1cf23e1912e92a61b333df1540
Rupam Das <dasrupam942@gmail.com>
2026-09-01 12:03:23 +0530

    Add Vercel deployment configuration

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import app from '../server/src/app.js';
+import { connectDB } from '../server/src/config/database.js';
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
