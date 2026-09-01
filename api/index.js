65e99d48b6e7971a48979757e5b3fc1b38259309
Rupam Das <dasrupam942@gmail.com>
2026-09-01 12:34:32 +0530

    Fix Express body parsing on Vercel by exporting app directly

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import app from '../server/src/app.js';
 import { connectDB } from '../server/src/config/database.js';
 
-export default async function handler(req, res) {
-  await connectDB();
-  return app(req, res);
-}
+// Mongoose automatically buffers queries until the connection is established
+connectDB();
+
+export default app;
